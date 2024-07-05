3727cbb14ad0d7ffc53d92c32400d6ad3358a288
James Mak <kitata2@gmail.com>
2024-07-05 14:12:14 +0800

    test

--- rs_report.py
+++ rs_report.py
@@ -8,12 +8,7 @@ def main():
     telegram_apikey = sys.argv[1]
     chat_id         = sys.argv[2]
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-token', type=str)
-    # parser.add_argument('-chatid', type=str)
-    # args = parser.parse_args()
-    
-    # Read the CSV file
+    # Read stock RS file
     with open('output/rs_stocks.csv', 'r') as file:
         reader = csv.reader(file)
         next(reader)
@@ -31,38 +26,36 @@ def main():
     print(result_list)
     
     results = ','.join(result_list)
-    message = "Stocks with RS rating > 80\n\n"
+    message = "Stocks with RS rating > 80 in past months\n\n"
     message += results
 
     message = urllib.parse.quote(message)
     url = f"https://api.telegram.org/{telegram_apikey}/sendMessage?chat_id={chat_id}&text={message}"
-
     requests.get(url)
 
-    # read rs_industries
+
+    # read Industries RS file
     with open('output/rs_industries.csv', 'r') as file2:
         reader2 = csv.reader(file2)
         next(reader2)
         data2 = list(reader2)
 
     # Filter the rows where the first three columns are all > 80
-    industry_filtered_data = [row for row in data2 if all(float(col) >= 90 for col in row[4:7])] #1-month and 3-month top
+    industry_filtered_data = [row for row in data2 if all(float(col) >= 90 for col in row[4:7])] # Percentile, 1-month and 3-month top
 
-    # Print the filtered rows
     industry_result_list = []
     for row in industry_filtered_data:
-        industry_result_list.append(f"{row[1]}-{row[2]} {row[4:7]}\n")
+        industry_result_list.append(f"{row[1]}-{row[2]} {row[4:7]}")
            
     
     print(industry_result_list)
     
-    industry_results = ','.join(industry_result_list)
+    industry_results = '\n'.join(industry_result_list)
     industry_message = "Stocks with Industry RS rating > 80\n\n"
     industry_message += industry_results
 
     industry_message = urllib.parse.quote(industry_message)
     url = f"https://api.telegram.org/{telegram_apikey}/sendMessage?chat_id={chat_id}&text={industry_message}"
-
     requests.get(url)
 
 
