84ef2c3ee17f7b407f7a6394c60d4417e6e54c87
James Mak <kitata2@gmail.com>
2024-07-05 13:18:28 +0800

    test

--- rs_report.py
+++ rs_report.py
@@ -36,7 +36,34 @@ def main():
 
     message = urllib.parse.quote(message)
     url = f"https://api.telegram.org/{telegram_apikey}/sendMessage?chat_id={chat_id}&text={message}"
-    print(url)
+
+    requests.get(url)
+
+    # read rs_industries
+    with open('output/rs_industries.csv', 'r') as file2:
+        reader2 = csv.reader(file2)
+        next(reader2)
+        data2 = list(reader2)
+
+    # Filter the rows where the first three columns are all > 80
+    industry_filtered_data = [row for row in data2 if all(float(col) > 80 for col in row[4:7])]
+
+    # Print the filtered rows
+    industry_result_list = []
+    for row in industry_filtered_data:
+        print(row[4:8])
+        industry_result_list.append(row[1]-row[2])
+           
+    
+    print(industry_result_list)
+    
+    industry_results = ','.join(industry_result_list)
+    industry_message = "Stocks with Industry RS rating > 80\n\n"
+    industry_message += industry_results
+
+    industry_message = urllib.parse.quote(industry_message)
+    url = f"https://api.telegram.org/{telegram_apikey}/sendMessage?chat_id={chat_id}&text={industry_message}"
+
     requests.get(url)
 
 
